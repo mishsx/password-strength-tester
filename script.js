1e1397644c41c0c8305ee5f42d995a5235ff8695
Shubham Mishra <mishrashubhamx@gmail.com>
2020-07-16 06:07:48 -0400

    Added logic to check presence of lowercase letters in string

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function updateStrengthMeter() {
 function calculatePasswordStrength(password) {
   const weaknesses = [];
   weaknesses.push(lengthWeakness(password));
+  weaknesses.push(lowerCaseWeakness(password));
   return weaknesses;
 }
 
@@ -39,6 +40,24 @@ function lengthWeakness(password) {
     return {
       message: "Your password could be longer",
       deduction: 15,
+    };
+  }
+}
+
+function lowerCaseWeakness(password) {
+  const matches = password.match(/[a-z]/g) || [];
+
+  if (matches.length === 0) {
+    return {
+      message: "Your password has no lowercase characters.",
+      deduction: 20,
+    };
+  }
+
+  if (matches.length <= 2) {
+    return {
+      message: "Your password could use more lowercase characters.",
+      deduction: 5,
     };
   }
 }
