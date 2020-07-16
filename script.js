f70dec4457e9b2360c1010206a5efa50580ebd92
Shubham Mishra <mishrashubhamx@gmail.com>
2020-07-16 01:46:19 -0400

    Variable declarations; Added logic to calculate password weakness

---
+++ script.js
@@ -0,0 +1,9 @@
+const strengthMeter = document.getElementById("strength-meter");
+const passwordInput = document.getElementById("password-input");
+const reasonsContainer = document.getElementById("reasons");
+
+function calculatePasswordStrength(password) {
+  const weaknesses = [];
+  weaknesses.push();
+  return weaknesses;
+}
